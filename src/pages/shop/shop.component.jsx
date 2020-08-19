46dcf507a0049082f0c162437fc29f9b09dd6b2b
Oriyomi Adepitan <oadepitan@softalliance.com>
2020-08-19 07:42:09 +0100

    created the collectionp review items

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { SHOP_DATA } from "./shop.data";
+import CollectionPreview from "./../../components/preview-collection/collection-preview.component";
+
+const ShopPage = () => {
+  const [collections, setCollections] = useState(SHOP_DATA);
+
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+export default ShopPage;
