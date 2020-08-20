8b9dbeaed883b47f8377674d7f0d05e09cc1be1e
Oriyomi Adepitan <oadepitan@softalliance.com>
2020-08-20 07:28:29 +0100

    Sign in with google component update

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -60,7 +60,11 @@ const SignIn = () => {
         />
         <div className="buttons">
           <CustomButton type="submit">Sign in</CustomButton>
-          <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
+          <CustomButton
+            type="button "
+            onClick={signInWithGoogle}
+            isGoogleSignIn
+          >
             Sign in with google
           </CustomButton>
         </div>
